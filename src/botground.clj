89450968488ab327bafe7140e0f87d5ed48db09f
pabrrs <paulo.alfredo.barros@gmail.com>
2020-07-11 19:59:24 -0300

    refac(log): Improve log message

--- src/botground.clj
+++ src/botground.clj
@@ -3,7 +3,7 @@
   (:use [clojure.java.shell :only [sh]]))
 
 (defn -main []
-  (println "Starting to download awesome wallpaper ...")
+  (println "Downloading awesome wallpaper 🤖")
   (sh "gsettings" "set" "org.gnome.desktop.background" "picture-uri" (unsplash/download-image))
-  (println "Finished! Enjoy your new wallpaper.")
+  (println "Enjoy your new wallpaper!")
   (System/exit 0))

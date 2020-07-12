ed8313cbf55539e06cf73e42da8d74995f387ac7
pabrrs <paulo.alfredo.barros@gmail.com>
2020-07-12 16:15:43 -0300

    refac(unsplash): udpate random image categories

--- test/botground.clj
+++ test/botground.clj
@@ -3,7 +3,7 @@
   (:use clojure.test))
 
 (deftest test-unsplash-random-image-url
-  (is (= unsplash/unsplash-random-image-url "https://source.unsplash.com/random/3840x2160?mountains,landscapes,houses,office,trees,cyberpunk,coding,art,buildings,nature,animals,sunset,sky,california")))
+  (is (= unsplash/unsplash-random-image-url "https://source.unsplash.com/random/3840x2160?mountains,trees,cyberpunk,coding,buildings,sunset,sky")))
 
 (deftest test-destination-file
   (is (= unsplash/destination-file (apply str (System/getenv "HOME") "/Pictures/Wallpapers/" "current-wallpaper.jpg"))))

beb375f8030c1d0c2780c87ed09c0240c9bcb295
pabrrs <paulo.alfredo.barros@gmail.com>
2020-07-13 12:36:44 -0300

    feat(unsplash): Add outdoors tag to image

--- test/botground.clj
+++ test/botground.clj
@@ -3,7 +3,7 @@
   (:use clojure.test))
 
 (deftest test-unsplash-random-image-url
-  (is (= unsplash/unsplash-random-image-url "https://source.unsplash.com/random/3840x2160?mountains,trees,cyberpunk,coding,buildings,sunset,sky")))
+  (is (= unsplash/unsplash-random-image-url "https://source.unsplash.com/random/3840x2160?mountains,trees,cyberpunk,coding,buildings,sunset,sky,outdoors")))
 
 (deftest test-destination-file
   (is (= unsplash/destination-file (apply str (System/getenv "HOME") "/Pictures/Wallpapers/" "current-wallpaper.jpg"))))
